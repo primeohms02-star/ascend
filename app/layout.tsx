128f0abd1906caa6d6805f1063f25e38fcf921ae
prime Huncho <YOUR_GITHUB_EMAIL@example.com>
2026-07-07 21:22:55 +0100

    Prepare production deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <ClerkProvider>
       <html lang="en">
